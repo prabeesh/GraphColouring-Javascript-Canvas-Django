87393c020835948f726acb2f5b20dada31242793
prabs <prabs@prabs.(none)>
2013-02-26 16:32:57 +0530

    django app

--- graph/settings.py
+++ graph/settings.py
@@ -1,5 +1,5 @@
 # Django settings for graph project.
-
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -101,8 +101,8 @@ ROOT_URLCONF = 'graph.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'graph.wsgi.application'
-
-TEMPLATE_DIRS = ('/home/prabs/djcode/graph/templates'
+PROJECT_DIR = os.path.dirname(__file__)
+TEMPLATE_DIRS = (os.path.join(PROJECT_DIR, "templates"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
